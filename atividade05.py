ae256af8e1ecd4d94aed815a25a1cb9bc6e64a7d
gabriellevims <gabriellevi2104@gmail.com>
2023-10-18 11:05:20 -0300

    cm e mm

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+# Digite uma metragem e veja isso em cm e mm:
+metro = float(input("digite metro:"))
+cm = metro*100
+mm = metro*1000
+print (f"metro em centímetro={cm}")
+print (f"metro em milímetro={mm}")
